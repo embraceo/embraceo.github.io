634da08326dbe1bc94626fb078a8872a68380087
embraceo <sheckley12@gmail.com>
2024-09-26 12:38:53 -0700

    Assignment 5
    Javascript Stuff

---
+++ Assignment 5/fancifymytext.js
@@ -0,0 +1,51 @@
+function biggerText() {
+	
+    document.getElementById("textInput").style.fontSize = "24pt";
+}
+
+function changeStyle() {
+	
+    let textArea = document.getElementById("textInput");
+	
+    if (document.getElementById("fancyShmancy").checked) {
+		
+        textArea.style.fontWeight = "bold";
+		
+        textArea.style.color = "blue";
+		
+        textArea.style.textDecoration = "underline";
+		
+    } 
+	
+	else {
+		
+        textArea.style.fontWeight = "normal";
+		
+        textArea.style.color = "black";
+		
+        textArea.style.textDecoration = "none";
+    }
+}
+
+function mooify() {
+	
+    let textArea = document.getElementById("textInput");
+	
+    let text = textArea.value.toUpperCase();
+	
+    let sentences = text.split(".");
+	
+    for (let i = 0; i < sentences.length; i++) {
+		
+        if (sentences[i].trim().length > 0) {
+			
+            let words = sentences[i].trim().split(" ");
+			
+            words[words.length - 1] += "-MOO";
+			
+            sentences[i] = words.join(" ");
+        }
+    }
+	
+    textArea.value = sentences.join(". ").trim();
+}
